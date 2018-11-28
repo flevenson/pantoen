7b79af447b4473a2f7e3837a0055508f48977ecb
“Freddie <freddielev@gmail.com>
2018-11-27 18:11:45 -0700

    Add CSS reset file and font, and some CSS

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const app = express();
 
 app.use(express.static('public'))
 
-app.get('/', (request, response) => {
-  response.send('hello world')
-})
 
 app.listen(3000, () => {
   console.log('Express running on localhost:3000')
