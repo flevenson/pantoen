0980d39a9302929c33e774270103f6e9ccf1184e
“Freddie <freddielev@gmail.com>
2018-11-27 20:07:19 -0700

    Add some more css

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+
 
 app.use(express.static('public'))
+
 
 
 app.listen(3000, () => {
